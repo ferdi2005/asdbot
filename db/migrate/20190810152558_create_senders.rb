bed9ff2fddb9665809aba18bcd7123db218e099c
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-08-11 00:32:04 +0200

    Update 20190810152558_create_senders.rb

--- db/migrate/20190810152558_create_senders.rb
+++ db/migrate/20190810152558_create_senders.rb
@@ -1,7 +1,7 @@
 class CreateSenders < ActiveRecord::Migration[5.2]
   def change
     create_table :senders do |t|
-      t.integer :chat_id, :limit => 18
+      t.integer :chat_id
       t.string :username
 
       t.timestamps
