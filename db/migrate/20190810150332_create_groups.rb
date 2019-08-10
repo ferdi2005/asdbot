da514e85b42a755fcf991d04f0649ed041239b5e
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-08-11 00:31:49 +0200

    big int

--- db/migrate/20190810150332_create_groups.rb
+++ db/migrate/20190810150332_create_groups.rb
@@ -1,7 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[5.2]
   def change
     create_table :groups do |t|
-      t.integer :chat_id, :limit => 18
+      t.bigint :chat_id
       t.string :username
 
       t.timestamps
