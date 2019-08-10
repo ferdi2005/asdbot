7645e98f26bd262153d154dc8bc0efdcc158aa90
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-08-11 00:50:30 +0200

    update_id

--- db/migrate/20190810152645_create_asds.rb
+++ db/migrate/20190810152645_create_asds.rb
@@ -3,7 +3,8 @@ class CreateAsds < ActiveRecord::Migration[5.2]
     create_table :asds do |t|
       t.references :group, foreign_key: true
       t.references :sender, foreign_key: true
-
+      t.integer :update_id
+      
       t.timestamps
     end
   end
