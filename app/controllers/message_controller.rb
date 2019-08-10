4c8feadaff1c787ff10c1c1d46bae47a8f017cdf
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-08-10 18:39:30 +0200

    fixie

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,10 +1,8 @@
 class MessageController < ApplicationController
   def message_process
     bot_api_key = '836213850:AAG-aBtJB8khJ53DNRlzORUVcFWOH5SOF9o'
-    if params[:token] == bot_api_key
       message = params[:message][:message]
       ProcessMessageJob.perform_later(message)
-    end
   end
 
   def classifica
