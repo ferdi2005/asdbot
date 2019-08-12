31091218e81740ebb4d6782f42a8ef357065c378
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-08-12 16:25:25 +0200

    unique chat_id

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
   has_many :asds
   has_many :special_events
+  validates :chat_id, uniqueness: true
 end
--- app/models/sender.rb
+++ app/models/sender.rb
@@ -1,3 +1,4 @@
 class Sender < ApplicationRecord
   has_many :asds
+  validates :chat_id, uniqueness: true
 end
