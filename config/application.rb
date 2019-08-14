17893777751ff8e1b70cd3f72fd2f4f8d8040812
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-08-14 15:20:55 +0200

    sidekiq per i job

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Asdbot
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = "Rome"
-
+    config.active_job.queue_adapter = :sidekiq
     Raven.configure do |config|
       config.dsn = 'https://0990a682fc9a4c2691cc14c1f81c6da3:99ef32af76c9457bb64bbad21305368a@sentry.io/1527516'
     end    
