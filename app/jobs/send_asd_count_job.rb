ec67e5024d7d0d54133ca064c0135c557f305a5e
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-12-23 14:23:36 +0100

    MANDA CONTO ASD SOLO SE QUESTO È MAGGIORE DI ZERO

--- app/jobs/send_asd_count_job.rb
+++ app/jobs/send_asd_count_job.rb
@@ -9,7 +9,7 @@ class SendAsdCountJob < ApplicationJob
       @asds = Asd.where(created_at: date.midnight..date.end_of_day, group: group)
       position = Group.all.sort_by{|gp| gp.asds.totalcount}.pluck(:id).reverse.find_index(@group.id) + 1
       defmultipletimes = @asds.pluck(:multiple_times).sum
-      if group.asds.count > 0 && !@group.silent
+      if @asds.count > 0 && !@group.silent
           Telegram.bot.send_message(chat_id: @group.chat_id, text: "È mezzanotte, ora di sapere! Il contasd di ieri conta ben #{@asds.count} (+#{defmultipletimes} multipli) per un totale di #{@asds.totalcount} asds, asd. Sei il #{position}º gruppo per ASD inviati. (Digita /silent per disattivarmi, non togliermi!)")  
       end
       rescue => e
